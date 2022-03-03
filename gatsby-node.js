d86b928a6634253cf75b8850d6ed445377fa3842
ArchawinWongkittiruk <willypoko@hotmail.com>
2022-03-03 16:01:21 +0000

    Minor refactor of version name assignment

--- gatsby-node.js
+++ gatsby-node.js
@@ -36,9 +36,8 @@ exports.createPages = ({ graphql, actions }) => {
 
     result.data.allSamplesJson.edges.forEach((edge) => {
       const { service, specName, generated } = edge.node;
-      let version = edge.node.version;
-      version =
-        service.toLowerCase() !== specName.toLowerCase() ? `${version} (${specName})` : version;
+      const serviceNameEqualsSpecName = service.toLowerCase() === specName.toLowerCase();
+      const version = edge.node.version + (serviceNameEqualsSpecName ? '' : ` (${specName})`);
 
       const serviceVersions = services.find((s) => s.service === service);
       if (serviceVersions) {
